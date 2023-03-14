781ddd745156c4ead5ee1df82feafd4d48aec47e
tejas4245 <120262626+tejas4245@users.noreply.github.com>
2023-03-15 00:37:47 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 // This file is not to be modified. Please ignore this.
 // We will understand all of this later in the course.
-// DO NOT MODIFY THIS FILE
+// DO NOT MODIFY THIS FIL
 
 const express = require('express');
 const path = require('path');
@@ -13,6 +13,15 @@ app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname + '/main.html'));
 });
 //your code here
+const players=['players1','players2','players3'];
+const person={
+  name:'john Doe',
+  age: 30,
+  gender:'male'
+};
+const team=players;
+const team=[...players];
+const team=[...players];
 app.post('/add', (req, res) => {
   const {a,b} = req.body;
   res.status(200).send(a+b);
